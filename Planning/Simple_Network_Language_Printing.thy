16f529e360c27d77f23e3b70e0d44277d0d3d929
David Wang <72378768+david-wang-0@users.noreply.github.com>
2025-02-05 16:27:36 +0100

    Implement preliminary version of printer for JSON

--- Planning/Simple_Network_Language_Printing.thy
+++ Planning/Simple_Network_Language_Printing.thy
@@ -5,32 +5,35 @@ theory Simple_Network_Language_Printing
     TA_Library.Error_List_Monad
 begin
 
-(* fun showsp_sbexp::"('n::show, 's::show, 'a::show, 'b::show) sexp \<Rightarrow> string \<Rightarrow> string" where
-"showsp_sbexp sexp.true = (@) ''true''" |
-"showsp_sbexp (sexp.not f) = shows ''(~'' o showsp_sbexp f o shows '')''" |
-"showsp_sbexp (sexp.and f g) = shows ''('' o showsp_sbexp f o shows '' && '' o showsp_sbexp g o shows '')''" |
-"showsp_sbexp (sexp.or f g) = shows ''('' o showsp_sbexp f o shows '' || '' o showsp_sbexp g o shows '')''" |
-"showsp_sbexp (sexp.imply f g) = shows ''('' o showsp_sbexp f o shows '' -> '' o showsp_sbexp g o shows '')''" |
-"showsp_sbexp (sexp.loc p s) = shows ''('' o shows p o shows ''.'' o shows s o shows '')''" |
-"showsp_sbexp (sexp.eq c d) = shows ''('' o shows c o shows '' = '' o shows d o shows '')''" |
-"showsp_sbexp (sexp.le c d) = shows ''('' o shows c o shows '' <= '' o shows d o shows '')''" |
-"showsp_sbexp (sexp.lt c d) = shows ''('' o shows c o shows '' < '' o shows d o shows '')''" |
-"showsp_sbexp (sexp.ge c d) = shows ''('' o shows c o shows '' >= '' o shows d o shows '')''" |
-"showsp_sbexp (sexp.gt c d) = shows ''('' o shows c o shows '' > '' o shows d o shows '')''"
+abbreviation print_list::"'b::show \<Rightarrow> ('a::show) list \<Rightarrow> string" where
+"print_list sep xs \<equiv> map show xs |> intersperse (show sep) |> concat"
 
+fun showsp_sbexp::"('n::show, 's::show, 'a::show, 'b::show) sexp \<Rightarrow> string \<Rightarrow> string" where
+"showsp_sbexp sexp.true = (@) ''true''" |
+"showsp_sbexp (sexp.not f) = shows ''~('' o showsp_sbexp f o shows '')''" |
+"showsp_sbexp (sexp.and f g) = shows ''('' o showsp_sbexp f o shows '') && ('' o showsp_sbexp g o shows '')''" |
+"showsp_sbexp (sexp.or f g) = shows ''('' o showsp_sbexp f o shows '') || ('' o showsp_sbexp g o shows '')''" |
+"showsp_sbexp (sexp.imply f g) = shows ''('' o showsp_sbexp f o shows '') -> ('' o showsp_sbexp g o shows '')''" |
+"showsp_sbexp (sexp.loc p s) = shows p o shows ''.'' o shows s" |
+"showsp_sbexp (sexp.eq c d) = shows c o shows '' = '' o shows d" |
+"showsp_sbexp (sexp.le c d) = shows c o shows '' <= '' o shows d" |
+"showsp_sbexp (sexp.lt c d) = shows c o shows '' < '' o shows d" |
+"showsp_sbexp (sexp.ge c d) = shows c o shows '' >= '' o shows d" |
+"showsp_sbexp (sexp.gt c d) = shows c o shows '' > '' o shows d"
+(*
 instantiation sexp::("show", "show", "show", "show") "show" begin
   definition "shows_prec_sexp (n::nat) e \<equiv> showsp_sbexp e"
  (*  definition "shows_list_sexp xs \<equiv> showsp_list (\<lambda>n x. showsp_sbexp x) 0 xs" *)
 instance sorry
 end *)
-(* 
-fun showsp_formula::"('n::show, 's::show, 'a::show, 'b::show) formula \<Rightarrow> string \<Rightarrow> string" where
-"showsp_formula (formula.EX b) = shows ''(E<> '' o showsp_sbexp b o shows '')''" |
-"showsp_formula (formula.EG b) = shows ''(E[] '' o showsp_sbexp b o shows '')''" |
-"showsp_formula (formula.AX b) = shows ''(A<> '' o showsp_sbexp b o shows '')''" |
-"showsp_formula (formula.AG b) = shows ''(A[] '' o showsp_sbexp b o shows '')''" |
-"showsp_formula (formula.Leadsto a b) = shows ''('' o showsp_sbexp a o shows '' --> '' o showsp_sbexp b o shows '')''"
 
+fun showsp_formula::"('n::show, 's::show, 'a::show, 'b::show) formula \<Rightarrow> string \<Rightarrow> string" where
+"showsp_formula (formula.EX b) = shows ''E<> '' o showsp_sbexp b" |
+"showsp_formula (formula.EG b) = shows ''E[] '' o showsp_sbexp b" |
+"showsp_formula (formula.AX b) = shows ''A<> '' o showsp_sbexp b" |
+"showsp_formula (formula.AG b) = shows ''A[] '' o showsp_sbexp b" |
+"showsp_formula (formula.Leadsto a b) = showsp_sbexp a o shows '' --> '' o showsp_sbexp b"
+(*
 instantiation formula::("show", "show", "show", "show") "show" begin
   definition "shows_prec_formula (n::nat) f \<equiv> showsp_formula f"
  (*  definition "shows_list_formula xs \<equiv> showsp_list (\<lambda>n x. showsp_formula x) 0 xs" *)
@@ -39,20 +42,17 @@ end
  *)
 
 (* For the goal. To do: locations are not just strings. *)
-definition formula_to_json::"(String.literal, String.literal, String.literal, int) formula \<Rightarrow> string \<times> JSON" where
-"formula_to_json f =
-  (''formula'', String (show f))
+definition formula_to_JSON::"(String.literal, String.literal, String.literal, int) formula \<Rightarrow> string \<times> JSON" where
+"formula_to_JSON f =
+  (''formula'', String (showsp_formula f ''''))
 "
 
 fun print_var::"(String.literal \<times> int \<times> int) \<Rightarrow> string" where
 "print_var (v, l, u) = show v @ ''['' @ show l @ '':'' @ show u @ '']''"
 
-definition print_var_list::"(String.literal \<times> int \<times> int) list \<Rightarrow> string" where
-"print_var_list vs = fold (\<lambda>l r. l @ '', '' @ r) (map print_var vs) ''''" 
-
 (* variables are passed together with bounds *)
 definition bounded_vars_to_vars_and_bounds::"(String.literal \<times> int \<times> int) list \<Rightarrow> string \<times> JSON" where
-"bounded_vars_to_vars_and_bounds bv \<equiv> (''vars'', String (print_var_list bv))"
+"bounded_vars_to_vars_and_bounds bv \<equiv> (''vars'', String (print_list '', '' (map print_var bv)))"
 
 
 (* What do actions do? They synchronise channels, but they are always paired with another value
@@ -95,7 +95,7 @@ and ('a, 'b) exp =
 (* Simple Network Language expressions use functions in the arguments. 
 I cannot think of a way to check whether the binary operator that is
 used to construct a bexp is (+), for instance. *)
-fun showsp_exp::"(('a::show), ('b::show)) exp \<Rightarrow> string \<Rightarrow> string" and
+(* fun showsp_exp::"(('a::show), ('b::show)) exp \<Rightarrow> string \<Rightarrow> string" and
   showsp_bexp::"('a, 'b::show) bexp \<Rightarrow> string \<Rightarrow> string" where
 "showsp_exp (exp.mult a b) = shows ''('' o showsp_exp a o shows '') * ('' o showsp_exp b o shows '')''" |
 "showsp_exp (exp.add a b) = shows ''('' o showsp_exp a o shows '') + ('' o showsp_exp b o shows '')''" |
@@ -111,23 +111,59 @@ fun showsp_exp::"(('a::show), ('b::show)) exp \<Rightarrow> string \<Rightarrow>
 "showsp_bexp (bexp.le x y) = shows ''('' o showsp_exp x o shows '') <= ('' o showsp_exp y o shows '')''" |
 "showsp_bexp (bexp.lt x y) = shows ''('' o showsp_exp x o shows '') < ('' o showsp_exp y o shows '')''" |
 "showsp_bexp (bexp.ge x y) = shows ''('' o showsp_exp x o shows '') >= ('' o showsp_exp y o shows '')''" |
-"showsp_bexp (bexp.gt x y) = shows ''('' o showsp_exp x o shows '') > ('' o showsp_exp y o shows '')''"
+"showsp_bexp (bexp.gt x y) = shows ''('' o showsp_exp x o shows '') > ('' o showsp_exp y o shows '')''" *)
+
+(* Why does show return a function? *)
+
+fun showsp_exp::"(('a::show), ('b::show)) exp \<Rightarrow> string" and
+  showsp_bexp::"('a, 'b::show) bexp \<Rightarrow> string" where
+"showsp_exp (exp.mult a b) = show ''('' @ showsp_exp a @ show '') * ('' @ showsp_exp b @ show '')''" |
+"showsp_exp (exp.add a b) = show ''('' @ showsp_exp a @ show '') + ('' @ showsp_exp b @ show '')''" |
+"showsp_exp (exp.if_then_else b x y) = show ''('' @ showsp_bexp b @ show '') ? ('' @ showsp_exp x @ show '') : ('' @ showsp_exp y @ show '')''" |
+"showsp_exp (exp.const c) = show c" |
+"showsp_exp (exp.var v) = show v" |
+"showsp_bexp bexp.true = show ''True''" |
+"showsp_bexp (bexp.not b) = show ''~'' @ showsp_bexp b" |
+"showsp_bexp (bexp.and a b) = show ''('' @ showsp_bexp a @ show '') && ('' @ showsp_bexp b @ show '')''" |
+"showsp_bexp (bexp.or a b) = show ''('' @ showsp_bexp a @ show '') || ('' @ showsp_bexp b @ show '')''" |
+"showsp_bexp (bexp.imply a b) = show ''('' @ showsp_bexp a @ show '') -> ('' @ showsp_bexp b @ show '')''" |
+"showsp_bexp (bexp.eq x y) =  showsp_exp x @ show '' = '' @ showsp_exp y" |
+"showsp_bexp (bexp.le x y) = showsp_exp x @ show '' <= '' @ showsp_exp y" |
+"showsp_bexp (bexp.lt x y) = showsp_exp x @ show '' < '' @ showsp_exp y" |
+"showsp_bexp (bexp.ge x y) = showsp_exp x @ show '' >= '' @ showsp_exp y" |
+"showsp_bexp (bexp.gt x y) = showsp_exp x @ show '' > '' @ showsp_exp y"
 
 instantiation exp:: ("show", "show") "show" begin
-  definition "shows_prec_exp (n::nat) e \<equiv> showsp_exp e"
-  definition "shows_list_exp xs \<equiv> showsp_list (\<lambda>n x. showsp_exp x) 0 xs"
-instance sorry
+  definition "shows_prec_exp (n::nat) e s \<equiv> (showsp_exp e) @ s"
+  definition "shows_list_exp xs s \<equiv> 
+    map showsp_exp xs |> intersperse '', '' |> (\<lambda>xs. ''['' @ concat xs @ '']'' @ s)"
+instance 
+  apply standard
+  subgoal for _ e s s'
+    apply (cases e)
+    unfolding shows_prec_exp_def by simp+
+  subgoal for xs s s'
+    unfolding shows_list_exp_def by simp+
+  done
 end
 
 (* To do: properly implement shows_*) 
 
 instantiation bexp:: ("show", "show") "show" begin
-  definition "shows_prec_bexp (n::nat) e \<equiv> showsp_bexp e"
-  definition "shows_list_bexp xs \<equiv> showsp_list (\<lambda>n x. showsp_bexp x) 0 xs"
-instance sorry
+  definition "shows_prec_bexp (n::nat) e s \<equiv> showsp_bexp e @ s"
+  definition "shows_list_bexp xs s \<equiv> 
+    map showsp_bexp xs |> intersperse '', '' |> (\<lambda>xs. ''['' @ concat xs @ '']'' @ s)"
+instance 
+  apply standard
+  subgoal for _ e s s'
+    apply (cases e)
+    unfolding shows_prec_bexp_def by simp+
+  subgoal for xs s s'
+    unfolding shows_list_bexp_def by simp+
+  done
 end
 
-fun update_to_str::"String.literal \<times> (String.literal, int) exp \<Rightarrow> string" where
+fun update_to_str::"'a::show \<times> ('v::show, 'c::show) exp \<Rightarrow> string" where
 "update_to_str (v, e) = show v @ show '' := '' @ show e"
 
 fun reset_to_str::"String.literal \<Rightarrow> string" where
@@ -156,17 +192,16 @@ fun sequence_list_opt::"'a option list \<Rightarrow> 'a list option" where
     Some (x # xs)
   }"
 
-fun traverse
 
+(* Guards are clock constraints. Invariants are expressions on variables *)
 definition inv_to_str::"(String.literal, int) acconstraint list 
   \<Rightarrow> (String.literal, int) bexp \<Rightarrow> string" where
-"inv_to_str guards invariants = (
+"inv_to_str guards invariants \<equiv>
   let 
-    cconsts = fold (\<lambda>l r. l @ show '' && '' @ r) (map show guards) '''';
-    invs = show invariants
-  in
-    cconsts @ show ''&& ('' @ invs @ show '')''
-)"
+    invariants = if (invariants = bexp.true) then '''' else show ''('' @ show invariants @ show '')'';
+    guard = print_list '' && '' (invariants # map show guards)
+  in guard"
+
 
 (* Simple_Network_Language_Export_Code.convert_edge *)
 definition edge_to_JSON::"
@@ -191,7 +226,7 @@ definition edge_to_JSON::"
     let upds = map update_to_str upds;
     let resets = map reset_to_str resets;
     let guards = (''guard'', String (inv_to_str guard check));
-    let updates = (''update'', String (fold (\<lambda>l r. l @ '', '' @ r) (upds @ resets) ''''));
+    let updates = (''update'', String (print_list '', '' (upds @ resets)));
     Result (Object [source, label, target, guards, updates])
   } |> err_msg (''Error while converting edge: '' @ show e |> String.implode)
 "
@@ -223,9 +258,9 @@ definition node_to_JSON::"
   id \<leftarrow> (case (name_to_id n) of 
     None \<Rightarrow> Error [''Location has no ID: '' @ (String.explode n) |> String.implode] |
     Some n \<Rightarrow> Result (''id'', JSON.Int n));
-  let f = (\<lambda>x. fold (\<lambda>l r. l @ show '' && '' @ r) (map show x) '''');
+  let f = (\<lambda>x. print_list '' && '' (map show x));
   let invs = default '''' (get (map_option f) (name_to_invs n));
-  let inv = (''inv'', String invs);
+  let inv = (''invariant'', String invs);
   let name = (''name'', String (show n));
   Result (Object [id, name, inv])
 }"
@@ -276,7 +311,7 @@ definition automaton_to_JSON::"
     
     init \<leftarrow> (case (name_to_id init) of 
       None \<Rightarrow> Error [''Location has no ID: '' @ (String.explode init) |> String.implode] |
-      Some n \<Rightarrow> Result (''init'', JSON.Int n));
+      Some n \<Rightarrow> Result (''initial'', JSON.Int n));
   
     edges \<leftarrow> edges_to_JSON name_to_id edges;
     let edges = (''edges'', edges);
@@ -305,7 +340,7 @@ definition automata_to_JSON::"(
   Result (JSON.Array automata)
 }"
 
-definition to_muntax::"
+definition net_to_JSON::"
   (String.literal \<times> 
     String.literal \<times> 
     String.literal list \<times> 
@@ -319,17 +354,21 @@ definition to_muntax::"
         String.literal) list \<times>
       (String.literal \<times> (String.literal, int) cconstraint) list) list \<times>
    (String.literal \<times> int \<times> int) list \<times>
+   String.literal list \<times>
    (String.literal, String.literal, String.literal, int) formula
   \<Rightarrow> JSON Error_List_Monad.result" where
-"to_muntax a \<equiv>
-  let (automata, vars, formula) = a
+"net_to_JSON a \<equiv>
+  let (automata, vars, clocks, formula) = a
   in 
   do {
     automata \<leftarrow> automata_to_JSON automata;
     let automata = (''automata'', automata);
-    Result (Object [automata, formula_to_json formula, bounded_vars_to_vars_and_bounds vars])
+    let clocks = (''clocks'', String (print_list '', '' clocks));
+    Result (Object [automata, formula_to_JSON formula, bounded_vars_to_vars_and_bounds vars, clocks])
   }
 " 
+
+(* No information about clocks in these. Extract clocks? *)
 (* The convert function extracts more information that is necessary for printing. To do: Implement
     simpler convert function for these types? *)
 
@@ -385,19 +424,28 @@ ML \<open>
   end
 \<close>
 
+ML \<open>
+  (* Write string to file *)
+  fun string_to_file s name = let
+    val f = TextIO.openOut name
+    val s = TextIO.output (f, s)
+    val _ = TextIO.closeOut f
+  in s end
+\<close>
+
 ML_val \<open>OS.FileSys.getDir()\<close>
 
 ML_val \<open>
   do_parse_convert "work/temporal_planning_certification/munta/certificates/planning.muntax"
 \<close>
 
-definition test_automaton1 where
-"test_automaton1 \<equiv> (
+definition goal_automaton where
+"goal_automaton \<equiv> (
   STR ''main'', 
   STR ''start'',
   [],
   [],
-  [(STR ''start'', 
+  [ (STR ''start'', 
     bexp.true, 
     [], 
     Sil (STR ''''), 
@@ -408,12 +456,65 @@ definition test_automaton1 where
       (STR ''s1'', const 1)
     ],
     [],
-    STR ''plan'')],
+    STR ''plan''),
+    (STR ''plan'', 
+    eq (var STR ''lock'') (const (1)), 
+    [], 
+    Sil (STR ''''), 
+    [
+      (STR ''lock'', const 0),
+      (STR ''pf11'', const 1),
+      (STR ''ef12'', const 1),
+      (STR ''s1'', const 1)
+    ],
+    [],
+    STR ''goal'')
+  ],
   []
 )"
 
-value [code]  "automaton_to_JSON test_automaton1"
+value [code] "automaton_to_JSON goal_automaton"
 
-definition test_to_muntax where
-"test_to_muntax \<equiv> ([])"
+
+definition vars where
+"vars \<equiv> [(STR ''pf11'', 0::nat, 1::nat), (STR ''ef11'', 0, 1), (STR ''s1'', 0::nat, 1::nat), (STR ''lock'', 0, 1)]"
+
+value [code] "bounded_vars_to_vars_and_bounds vars"
+
+definition clocks where
+"clocks \<equiv> []"
+
+definition form where
+"form \<equiv> (formula.EX (loc (STR ''main'') (STR ''goal'')))"
+
+
+value [code] "do {
+auto \<leftarrow> automaton_to_JSON goal_automaton;
+let s = show auto;
+s |> Result
+}"
+
+value [code] "formula_to_JSON form"
+
+definition [code]: "network \<equiv> (
+  [goal_automaton], vars, clocks, form
+)
+"
+
+definition [code]: "net_to_string net \<equiv> 
+(case net_to_JSON net of 
+  Result j \<Rightarrow> show j
+| Error e \<Rightarrow> show e) |> String.implode"
+
+
+definition "test_net \<equiv> net_to_string network"
+
+ML_val \<open>OS.FileSys.getDir()\<close>
+
+ML_val \<open>
+string_to_file @{code "test_net"} "work/temporal_planning_certification/munta/test_output/net.json"
+\<close>
+
+
+
 end
